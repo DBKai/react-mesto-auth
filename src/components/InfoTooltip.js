8d6f1b0757c0099204fc9db74a34c776c98dc4c5
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-07-10 17:47:25 +0500

    feat: added infotooltip component

---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,28 @@
+import SuccessIcon from "../images/status_success.svg";
+import ErrorIcon from "../images/status_error.svg";
+
+function InfoTooltip({isOpen, onClose, isSuccess}) {
+  const successStatusText = "Вы успешно зарегистрировались!";
+  const errorStatusText = "Что-то пошло не так! Попробуйте ещё раз.";
+
+  function handleClosePopup(event) {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  }
+
+  return(
+    <div className={`popup ${isOpen && 'popup_opened'}`} onMouseDown={handleClosePopup}>
+      <div className="popup__container tooltip__container">
+        <button className="popup__close"
+                type="button"
+                aria-label="Закрыть попап"
+                onClick={onClose}></button>
+        <img className="tooltip__icon" src={isSuccess ? SuccessIcon : ErrorIcon} alt="Статус"/>
+        <h2 className="tooltip__heading">{isSuccess ? successStatusText : errorStatusText}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default InfoTooltip;
